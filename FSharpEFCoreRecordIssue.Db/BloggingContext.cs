bc56f3ed535d4c1ccdcdebe5d309fb42241ce535
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2021-07-12 20:57:04 +0800

    Add project files.

---
+++ FSharpEFCoreRecordIssue.Db/BloggingContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FSharpEFCoreRecordIssue.Db
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=test.db");
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public int Rating { get; set; }
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
---
+++ FSharpEFCoreRecordIssue/Program.fs
@@ -0,0 +1,42 @@
+﻿open System.Linq
+open Microsoft.EntityFrameworkCore
+open FSharpEFCoreRecordIssue.Db
+
+
+let db = new BloggingContext()
+db.Database.Migrate()
+
+
+if db.Blogs.Count() = 0 then
+    let blogs = 
+        [|
+            for i in 1..3 do
+                Blog
+                    (Url = $"url {i}"
+                    ,Rating = System.Random().Next(1, 10)
+                    ,Posts =
+                        [|
+                            for j in 1..2 do
+                                Post
+                                    (Title = $"Title {j}"
+                                    ,Content = $"Content {j}")
+                        |])
+        |]
+
+    db.Blogs.AddRange blogs |> ignore
+    db.SaveChanges() |> ignore
+
+
+let tryRun fn =
+    try 
+        printfn "========================================================"
+        fn() |> Seq.iter (printfn "%A")
+        printfn "========================================================"
+    with ex ->
+        printfn $"Error: {ex.Message}"
+
+
+tryRun (fun () -> db.Blogs.AsNoTracking().Select(fun x -> {| Rating = x.Rating; Url = x.Url |}))
+tryRun (fun () -> db.Blogs.AsNoTracking().Select(fun x -> {| Url = x.Url; Rating = x.Rating |}))
+tryRun (fun () -> db.Blogs.AsNoTracking().Select(fun x -> {| Url = x.Url; PostCount = x.Posts.Count |}))
+tryRun (fun () -> db.Blogs.AsNoTracking().Select(fun x -> {| PostCount = x.Posts.Count; Url = x.Url |}))
